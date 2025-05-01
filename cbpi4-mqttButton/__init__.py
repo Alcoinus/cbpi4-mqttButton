01e0103ebbd69a1dfc7494ef0004706e03211700
Felix Theiß <felix.theiss@outlook.com>
2025-05-01 17:17:26 +0200

    first commit

---
+++ cbpi4-mqttButton/__init__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import logging
+import asyncio
+from cbpi.api import *
+
+
+logger = logging.getLogger(__name__)
+
+@parameters(
+    [
+        Property.Text(label="Topic", configurable=True,
+                      description="MQTT Topic"),
+        Property.Text(
+            label="Payload",
+            configurable=True,
+            description="Payload that is sent as MQTT message. Defaults to \"PRESSED\""
+        )
+    ])
+class MQTTButton(CBPiActor):
+    def __init__(self, cbpi, id, props):
+        super(MQTTButton, self).__init__(cbpi, id, props)
+
+    async def on_start(self):
+        self.topic = self.props.get("Topic", None)
+        self.payload = self.props.get("Payload", "PRESSED")
+        self.state = False
+
+    async def on(self, power=None):
+        await self.cbpi.satellite.publish(
+            self.topic, self.payload, False
+        )
+        self.state = True
+
+    async def off(self):
+        self.state = False
+
+    async def run(self):
+        while self.running:
+            if self.state:
+                await self.cbpi.actor.off(self.id)
+            await asyncio.sleep(1)
+
+    def get_state(self):
+        return self.state
+    
+
+def setup(cbpi):
+    if str(cbpi.static_config.get("mqtt", False)).lower() == "true":
+        cbpi.plugin.register("MQTTButton", MQTTButton)
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(name='cbpi4-mqttButton',
+      version='0.0.1',
+      description='CraftBeerPi Plugin to add an MQTT Button',
+      author='Felix Theiß',
+      author_email='felix.theiss@outlook.com',
+      url='',
+      include_package_data=True,
+      package_data={
+        # If any package contains *.txt or *.rst files, include them:
+      '': ['*.txt', '*.rst', '*.yaml'],
+      'cbpi4-mqttButton': ['*','*.txt', '*.rst', '*.yaml']},
+      packages=['cbpi4-mqttButton'],
+      long_description=long_description,
+      long_description_content_type='text/markdown'
+     )
